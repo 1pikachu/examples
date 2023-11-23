96c372276bfb81f815a0da02bf86034f241d96c2
jun.du@intel.com <jun.du@intel.com>
2023-11-22 20:47:40 -0800

    update launch_benchmark.sh

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -9,7 +9,7 @@ function main {
     set_environment
 
     # requirements
-    pip install expecttest==0.1.4 geffnet==1.0.2
+    pip install expecttest==0.1.4 geffnet==1.0.2 --no-deps
 
     # if multiple use 'xxx,xxx,xxx'
     model_name_list=($(echo "${model_name}" |sed 's/,/ /g'))
